1525bcc042b6728620f962ab1d3ae575a3da6921
aeremin <aeremin@maxus.ru>
2015-07-22 12:05:42 +0300

    done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,43 @@
 class Genre
-  # code here
-
-  def self.reset_all
+  # class
+  @@genres = []
+  
+  def self.count
+    @@genres.size
   end
+  
+  def self.all
+    @@genres
+  end
+  
+  def self.find_by_name(name)
+    @@genres.each{ |genre| return genre if genre.name == name }
+  end
+  
+  def self.create_by_name(name)
+    new_genre = self.new
+	new_genre.name = name
+	new_genre
+  end
+  
+  def self.reset_all
+    @@genres.clear
+  end
+  
+  # instance
+  attr_accessor :name
+  
+  def initialize
+    @@genres << self
+	@songs = []
+	@artists = []
+  end
+
+  def songs
+    @songs
+  end	
+  
+  def artists
+    @artists
+  end	
 end
--- app/models/library_parser.rb
+++ app/models/library_parser.rb
@@ -1,3 +1,25 @@
 class LibraryParser
   # code here
+  def files
+    Dir['db/data/*'].select { |e| File.file?(e) }
+  end
+  
+  def parse_filename(name)
+    match = /^(.+)\s\-\s(.+)\s\[(.+)\]\.mp3$/.match(name)
+	[match[1], match[2], match[3]]
+  end
+  
+  def build_song(artist_name, song_name, genre_name)
+	song = Song.create_by_name(song_name)
+	song.artist = Artist.create_by_name(artist_name)
+	song.genre = Genre.create_by_name(genre_name)
+	song
+  end
+  
+  def call
+    files.each do |file| 
+	  artist_name, song_name, genre_name = parse_filename(file)
+	  build_song(artist_name, song_name, genre_name)
+	end  
+  end
 end
