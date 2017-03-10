7ee1a9bbbb5c9cd9c9811db96454031a629d84bc
pkprosol <peter.prosol@gmail.com>
2017-03-10 13:15:01 +0530

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,40 @@
+require 'pry'
+
 class Artist
-  # code here
+  extend Findable
+
+  attr_accessor :name, :songs
+
+  ARTISTS = []
+
+  def initialize
+    ARTISTS << self
+    @songs = []
+    @genres = []
+  end
 
   def self.reset_all
+    ARTISTS.clear
+  end
+
+  def self.all
+    ARTISTS
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+  def genres
+    self.songs.map { |song| song.genre }.uniq
   end
 end
