051025484bf73597a59aaae3c02608d2612aa137
OnlyHughMan <hughvidler@hotmail.com>
2017-06-24 18:31:30 +1000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,46 @@
 class Genre
-  # code here
+	attr_accessor :name, :songs
+	@@genres = []
 
-  def self.reset_all
-  end
+	def initialize
+		@@genres << self
+		@artists = []
+		@songs = []
+	end
+
+	def add_song(song)
+		@songs << song
+	end
+
+	def add_artist(artist)
+		@artists << artist
+	end
+
+	def artists
+		@artists.uniq
+	end
+	
+	def self.all
+		@@genres
+	end
+
+	def self.count
+		@@genres.count
+	end
+
+	def self.reset_all
+		@@genres = []
+	end
+
+	def self.find_by_name(genreName)
+		@@genres.find{|genre| genre.name == genreName}
+	end
+
+	def self.create_by_name(genreName)
+		genre = Genre.new
+		genre.name = genreName
+		genre
+	end
+
+
 end
